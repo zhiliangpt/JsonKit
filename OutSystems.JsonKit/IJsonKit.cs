00e5c91d83c2e2e4cadb72fedef1b4aacfb5c44d
Zhi Liang <zhi.liang@outsystems.com>
2024-06-14 00:27:15 +0100

    Created Beautify.

---
+++ OutSystems.JsonKit/IJsonKit.cs
@@ -0,0 +1,15 @@
+﻿using OutSystems.ExternalLibraries.SDK;
+using System.Security.Cryptography;
+
+namespace OutSystems.JsonKit
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[OSInterface(Description = "JSON library in OutSystems Developer Cloud (ODC) apps.", IconResourceName = "OutSystems.JsonKit.resources.jsonkit.png")]
+	public interface IJsonKit
+	{
+
+		void Beautify(string jsonIn, out string jsonOut);
+	}
+}
---
+++ OutSystems.JsonKit/JsonKit.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System;
+using Newtonsoft.Json.Linq;
+
+namespace OutSystems.JsonKit
+{
+	public class JsonKit : IJsonKit
+	{
+		public void Beautify(string jsonIn, out string jsonOut)
+		{
+			//Console.WriteLine(JToken.Parse(t).ToString(Newtonsoft.Json.Formatting.Indented));
+			jsonOut = string.Empty;
+
+			JToken jt = JToken.Parse(jsonIn);
+			jsonOut = jt.ToString(Newtonsoft.Json.Formatting.Indented);
+		}
+	}
+}
